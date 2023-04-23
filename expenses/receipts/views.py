b8b61347bc9630d3cdc016f6d5e9f382dccd9dc3
Jay <santana.jazel423@gmail.com>
2023-04-22 23:52:25 -0400

    created html list and base page

--- expenses/receipts/views.py
+++ expenses/receipts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
+from receipts.models import ExpenseCategory,Receipt,Account
 
 # Create your views here.
+
+def receipt_list(request):
+    receipt = Receipt.objects.all()
+    context = {
+        "receipt": receipt
+    }
+    return render(request, 'receipts/list.html', context)
